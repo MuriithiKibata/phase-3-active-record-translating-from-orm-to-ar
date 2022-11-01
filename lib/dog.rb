aae1243f0f44c891e85feaf35366f985b4b95ed0
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-11-01 12:17:18 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,13 @@
-class Dog
+class Dog < ActiveRecord::Base
+    # attr_accessor :name, :breed
+    # def initialize(name: "Fido", breed: "lab")
+    # @name = name
+    # @breed = breed
+    # end
+
+    # Student.create(name: "Ralph" breed: "lab")
+    # def self.create(name:, breed:)
+        
+    # end
+
 end
